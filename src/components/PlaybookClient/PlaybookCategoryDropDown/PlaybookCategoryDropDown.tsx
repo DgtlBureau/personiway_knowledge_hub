29a85db1a5dfeeac605c92089e1eca5e8b172c50
alfat <gilmalf89@gmail.com>
2025-03-12 17:29:57 +0500

    fix: rename all categories title

--- src/components/PlaybookClient/PlaybookCategoryDropDown/PlaybookCategoryDropDown.tsx
+++ src/components/PlaybookClient/PlaybookCategoryDropDown/PlaybookCategoryDropDown.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import {
-    underscopeFormatter,
-    underscopeReverter
+  underscopeFormatter,
+  underscopeReverter,
 } from '@/src/utils/formatter/underscopeFormatter';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
@@ -58,7 +58,7 @@ export const PlaybookCategoryDropDown = ({ categories }: ICategoryProps) => {
         Categories:{' '}
         {pathname && pathnameArr.length === 1 ? (
           <span className='font-bold capitalize leading-[1.3] text-main-blue'>
-            playbook
+            all
           </span>
         ) : pathnameArr.length === 2 && !subCategory ? (
           <span className='font-bold capitalize leading-[1.3] text-main-blue'>
@@ -79,14 +79,14 @@ export const PlaybookCategoryDropDown = ({ categories }: ICategoryProps) => {
         }}
       >
         <ul
-          className={`flex flex-col gap-[10px] overflow-hidden p-[4px] font-proxima capitalize`}
+          className={`flex flex-col gap-[10px] overflow-hidden overflow-scroll p-[4px] font-proxima capitalize`}
         >
           <li className={`relative`}>
             <Link
-              href='/hvac'
+              href='/'
               className={`block w-full rounded-[8px] p-[12px_16px] text-left text-[14px] capitalize leading-[1.4] text-[#525760] hover:bg-[#f5f5f6] ${pathnameArr.length === 1 ? 'bg-[#f5f5f6] font-bold' : ''}`}
             >
-              hvac
+              All
             </Link>
             <span className='absolute bottom-[-5px] block h-[2px] w-full translate-y-[50%] bg-main-disabled' />
           </li>
@@ -96,7 +96,7 @@ export const PlaybookCategoryDropDown = ({ categories }: ICategoryProps) => {
               className={`relative flex flex-col gap-[4px]`}
             >
               <Link
-                href={`/hvac/${item.category}${item.subCategory.length > 1 ? '' : `?sub-category=${underscopeFormatter(item.subCategory[0]).toLowerCase()}`}`}
+                href={`/${item.category}${item.subCategory.length > 1 ? '' : `?sub-category=${underscopeFormatter(item.subCategory[0]).toLowerCase()}`}`}
                 className={`relative w-full rounded-[8px] p-[12px_16px] text-left text-[14px] capitalize leading-[1.4] text-[#525760] hover:bg-[#f5f5f6] ${pathnameArr.includes(item.category.trim().toLowerCase()) ? 'bg-[#f5f5f6] font-bold' : ''} `}
               >
                 {item.category}
@@ -105,7 +105,7 @@ export const PlaybookCategoryDropDown = ({ categories }: ICategoryProps) => {
                 {item.subCategory.map((el) => (
                   <li key={el} className={`flex flex-col gap-[2px]`}>
                     <Link
-                      href={`/hvac/${item.category}?sub-category=${underscopeFormatter(el).toLowerCase()}`}
+                      href={`/${item.category}?sub-category=${underscopeFormatter(el).toLowerCase()}`}
                       className={`w-full rounded-[8px] p-[12px_26px] text-left text-[14px] capitalize leading-[1.4] text-[#525760] hover:bg-[#f5f5f6] ${el && subCategory && underscopeReverter(subCategory).toLowerCase() === el.trim().toLowerCase() ? 'bg-[#f5f5f6] font-bold' : ''}`}
                     >
                       {el}
