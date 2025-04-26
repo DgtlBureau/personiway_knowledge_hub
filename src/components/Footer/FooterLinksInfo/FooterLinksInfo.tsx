1245a18e804730b8a801c251a8a22d79e8468bfc
Alfat1989 <Alfat1989@users.noreply.github.com>
2025-04-26 23:18:41 +0500

    fix: change footer email

--- src/components/Footer/FooterLinksInfo/FooterLinksInfo.tsx
+++ src/components/Footer/FooterLinksInfo/FooterLinksInfo.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-// import LightMainLogo from '@/public/assets/images/icons/footer_logo_dark.svg';
 import LinkedInIcon from '@/public/assets/images/icons/linkedin.svg';
 import useMediaQuery from '@/src/utils/useMediaQuery';
 import Link from 'next/link';
@@ -12,9 +11,6 @@ export const FooterLinksInfo = () => {
     <div className='flex w-full flex-col justify-between'>
       <div className='flex flex-col text-white'>
         <div className='flex items-center justify-between'>
-          {/* <Link aria-label='Go to the main page' href='/'>
-            <LightMainLogo className='h-[54px] w-[auto] tablet:h-[80px] desktop:h-[92px]' />
-          </Link> */}
           {isMobile && (
             <Link
               target='_blank'
@@ -27,10 +23,10 @@ export const FooterLinksInfo = () => {
         </div>
         <div className='mt-[24px] grid w-full grid-cols-1 tablet:mt-[40px] laptop:mt-[80px] laptop:grid-cols-2'>
           <Link
-            href='mailto:access@thebrightbyte.com'
+            href='mailto:info@personiway.com'
             className='flex h-full w-full items-center font-proxima text-[18px] font-bold text-white tablet:text-[20px] laptop:order-last laptop:justify-end desktop:text-[26px]'
           >
-            access@thebrightbyte.com
+            info@personiway.com
           </Link>
           <AddressInfo />
         </div>
@@ -40,7 +36,7 @@ export const FooterLinksInfo = () => {
           <Link
             target='_blank'
             rel='noopener'
-            href='https://www.linkedin.com/company/thebrightbyte/'
+            href='https://www.linkedin.com/company/personiway'
           >
             <LinkedInIcon className='h-[32px] w-[auto]' />
           </Link>
