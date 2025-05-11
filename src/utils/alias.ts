483c863a0350c1573b940c1285404231c0636e56
Vitaliy <gronzzz@gmail.com>
2025-05-11 13:34:04 -0400

    Update alias.ts

--- src/utils/alias.ts
+++ src/utils/alias.ts
@@ -1,7 +1,7 @@
 export const BASE_URL = 'http://hub.personiway.com';
-export const seoSiteName = 'Personiway.com';
+export const seoSiteName = 'hub.personiway.com';
 export const defaultSeoAlternatesTitle =
-  'PersoniWay - HVAC Controllers & Automation Knowledge Hub';
+  'HVAC & BMS Knowledge Hub – Diagrams, Controllers & AI Support | hub.personiWay.com';
 export const GTM_Number = 'K5GHTXPD';
 export const maxSeoDescriptionSize = 155;
 export const postFolderPath = 'src/posts';
